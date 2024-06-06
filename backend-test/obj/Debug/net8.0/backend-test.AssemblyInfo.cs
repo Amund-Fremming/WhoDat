eb5ccd597289bcaa7e1177be92a92710544913b0
amund.fremming <amund.fremming@gmail.com>
2024-06-06 09:33:59 +0200

    Yeah

--- backend-test/obj/Debug/net8.0/backend-test.AssemblyInfo.cs
+++ backend-test/obj/Debug/net8.0/backend-test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1701d41a8a8b796e4b43fc87ebc04e25363fc577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4550e5743a31337a60da89895f4f903cb6690")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1701d41a8a8b796e4b43fc87ebc04e25363fc577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4550e5743a31337a60da89895f4f903cb6690")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
