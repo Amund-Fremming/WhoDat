ca43dd5a10d0950a9d972c9b01d99b67b6a6e6e0
amund-fremming <amund.fremming@gmail.com>
2024-06-18 17:31:16 +0200

    Added more test

--- backend-test/BoardCard/BoardCardTest.cs
+++ backend-test/BoardCard/BoardCardTest.cs
@@ -2,6 +2,38 @@ namespace BoardCardEntity;
 
 public class BoardCardTest
 {
+    public readonly Mock<ILogger<IBoardCardService>> _mockLogger;
+    public readonly Mock<IBoardCardRepository> _mockBoardCardRepository;
+    public readonly Mock<IBoardRepository> _mockBoardRepository;
+    public readonly Mock<ICardRepository> _mockCardRepository;
+    public readonly Mock<IGameRepository> _mockGameRepository;
+    public readonly IBoardCardService _boardCardService;
+    public readonly AppDbContext _context;
+
+    public BoardCardTest()
+    {
+        _mockLogger = new Mock<ILogger<IBoardCardService>>();
+        _mockBoardCardRepository = new Mock<IBoardCardRepository>();
+        _mockBoardRepository = new Mock<IBoardRepository>();
+        _mockCardRepository = new Mock<ICardRepository>();
+        _mockGameRepository = new Mock<IGameRepository>();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("TestDatabase")
+            .Options;
+
+        _context = new AppDbContext(options);
+
+        _boardCardService = new BoardCardService(
+                _context,
+                _mockLogger.Object,
+                _mockBoardCardRepository.Object,
+                _mockBoardRepository.Object,
+                _mockCardRepository.Object,
+                _mockGameRepository.Object
+                );
+    }
+
     // CreateBoardCards_Successful_PlayerHasPermission
     // CreateBoardCards_GameDoesNotExist_ShouldThrow
     // CreateBoardCards_PlayerHasNotPermission_ShouldThrow
@@ -9,13 +41,101 @@ public class BoardCardTest
 
     ///
 
-    // UpdateBoardCardsActivity_Successful_PlayerHasPermission
-    // UpdateBoardCardsActivity_BoardDoesNotExist_ShouldThrow
-    // UpdateBoardCardsActivity_PlayerHasNotPermission_ShouldThrow
+    public async Task UpdateBoardCardsActivity_Successful_PlayerHasPermission()
+    {
+        // Arrange
+        int playerId = 12;
+        int boardId = 22;
+        IEnumerable<BoardCardUpdate> boardCardUpdates;
+
+        // Act
+
+        // Assert
+    }
+
+    public async Task UpdateBoardCardsActivity_BoardDoesNotExist_ShouldThrow()
+    {
+        // Arrange
+        int playerId = 12;
+        int boardId = 22;
+        IEnumerable<BoardCardUpdate> boardCardUpdates;
+
+        // Act
+
+        // Assert
+    }
+
+    public async Task UpdateBoardCardsActivity_PlayerHasNotPermission_ShouldThrow()
+    {
+        // Arrange
+        int playerId = 12;
+        int boardId = 22;
+        IEnumerable<BoardCardUpdate> boardCardUpdates;
+
+        // Act
+
+        // Assert
+    }
+
 
     ///
 
-    // GetBoardCardsFromBoard_Successful_PlayerHasPermission
-    // GetBoardCardsFromBoard_BoardDoesNotExist_ShouldThrow
-    // GetBoardCardsFromBoard_PlayerHasNotPermission_ShouldThrow
+    public async Task GetBoardCardsFromBoard_Successful_PlayerHasPermission()
+    {
+        // Arrange
+        int playerId = 12;
+        int boardId = 22;
+        int gameId = 33;
+
+        Board board = new Board(playerId, gameId);
+        board.BoardID = boardId;
+        _mockBoardRepository.Setup(repo => repo.GetBoardById(boardId))
+            .ReturnsAsync(board);
+
+        IList<BoardCard> boardCards = new List<BoardCard>
+        {
+            new BoardCard(),
+            new BoardCard(),
+            new BoardCard(),
+        };
+        _mockBoardCardRepository.Setup(repo => repo.GetBoardCardsFromBoard(boardId))
+            .ReturnsAsync(boardCards);
+
+        // Act
+        IEnumerable<BoardCard> result = await _boardCardService.GetBoardCardsFromBoard(playerId, boardId);
+
+        // Assert
+        Assert.Equal(boardCards, result.ToList());
+    }
+
+    public async Task GetBoardCardsFromBoard_BoardDoesNotExist_ShouldThrow()
+    {
+        // Arrange
+        int playerId = 12;
+        int boardId = 22;
+
+        _mockBoardRepository.Setup(repo => repo.GetBoardById(boardId))
+            .ThrowsAsync(new KeyNotFoundException($"Board with id {boardId}, does not exist!"));
+
+        // Act and Assert
+        await Assert.ThrowsAsync<KeyNotFoundException>(() => _boardCardService.GetBoardCardsFromBoard(playerId, boardId));
+    }
+
+    public async Task GetBoardCardsFromBoard_PlayerHasNotPermission_ShouldThrow()
+    {
+        // Arrange
+        int playerId = 12;
+        int ownerPlayerId = 13;
+        int boardId = 22;
+        int gameId = 33;
+
+        Board board = new Board(ownerPlayerId, gameId);
+        board.PlayerID = ownerPlayerId;
+        _mockBoardRepository.Setup(repo => repo.GetBoardById(boardId))
+            .ReturnsAsync(board);
+
+        // Act and Assert
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _boardCardService.GetBoardCardsFromBoard(playerId, boardId));
+    }
+
 }
--- backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7fad652763c88fdb7a2822ef4c322ddd1adea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138d7fd983aa69fcb6eb186fd799297320c67fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
