c42dd2b70110dc828af16bd4bdf85d3f81aa8716
amund.fremming <amund.fremming@gmail.com>
2024-07-01 10:54:22 +0200

    jly

--- frontend/util/InputValitator.ts
+++ frontend/util/InputValitator.ts
@@ -1,7 +1,10 @@
-export const validText = (input: string) => {
-  // TODO
+const textRegex = /^[A-Za-z]+$/;
+const numberRegex = /^[0-9]+$/;
+
+export const validText = (input: string): boolean => {
+  return textRegex.test(input);
 };
 
-export const validNumber = (input: string) => {
-  // TODO
+export const validNumber = (input: string): boolean => {
+  return numberRegex.test(input);
 };
