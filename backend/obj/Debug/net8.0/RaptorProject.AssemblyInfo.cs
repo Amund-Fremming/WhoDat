171602f41616ffbe8eee3b508277de0dfaaaef63
amund.fremming <amund.fremming@gmail.com>
2024-07-14 13:32:07 +0200

    bin and obj

--- backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072407744c398c9e0302960531cdaecbd1d1fc90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fb805359b05e22d02f17b4b45bfa4bde7f3603")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
