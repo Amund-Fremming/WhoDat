467d099b3d6e4ad6915e714fa4e2b8598052abd8
amund-fremming <amund.fremming@gmail.com>
2024-09-07 16:38:53 +0200

    more shit

--- backend-test/obj/Debug/net8.0/backend-test.AssemblyInfo.cs
+++ backend-test/obj/Debug/net8.0/backend-test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fb4bb7c7d3b048f600a0c04f3a067e6b219749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d57917834992c689f50eadf60fbaf0b0005f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
