eabd15fe7fff60ef1640bad8d4a24dc7450d6e44
amund.fremming <amund.fremming@gmail.com>
2024-10-03 19:31:42 +0200

    trying something in another branch

--- backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbc64004a0e41371753a1f0a934d84b0a392dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abc42b70d9d04c7027fbd53617c279dfddd6655")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
