6427701661fe23ced0c50063d13db06577a35fe0
amund.fremming <amund.fremming@gmail.com>
2024-10-05 15:28:28 +0200

    it fucking works

--- backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RaptorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabd15fe7fff60ef1640bad8d4a24dc7450d6e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c19091c3c9e34a5f3e5cdbc885f7d451e82b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaptorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
