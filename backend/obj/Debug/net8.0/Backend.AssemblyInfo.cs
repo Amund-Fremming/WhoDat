ce3b34831364fa9291b0374dcf91dc94a966d679
Amund-fremming <amund.fremming@gmail.com>
2024-11-26 12:54:30 +0100

    Add new Backend project and update related configurations
    Updated backend.sln to include the new Backend project and removed RaptorProject configurations. Updated Backend.AssemblyInfo.cs with a new version identifier. Refreshed Backend.AssemblyInfoInputs.cache with a new hash value. Updated Backend.sourcelink.json to point to a new commit. Adjusted project.nuget.cache to reflect the new project file path for Backend.csproj.

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed993520fd605319da4262d2a5561b26ee633c89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3eb552688b930061a2051f172ff65b607e4c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
