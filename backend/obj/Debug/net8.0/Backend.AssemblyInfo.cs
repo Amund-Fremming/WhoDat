3b3eb552688b930061a2051f172ff65b607e4c06
Amund-fremming <amund.fremming@gmail.com>
2024-11-26 12:51:26 +0100

    Fuck it we ball

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ea2ef6295578604b39adde6a30583437dd95aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed993520fd605319da4262d2a5561b26ee633c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
