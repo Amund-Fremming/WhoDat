a2fc397c7e469708129504a5c0235d5d7514a28c
Amund-fremming <amund.fremming@gmail.com>
2024-11-27 10:16:58 +0100

    New yml file

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3eb552688b930061a2051f172ff65b607e4c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57903072ab526e5278fbde972e35257a1963dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
