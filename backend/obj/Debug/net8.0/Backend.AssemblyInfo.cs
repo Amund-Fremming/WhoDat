f15ebf8318f6ed47b3d5036d6bf79f0ead3cdfe2
Amund-fremming <amund.fremming@gmail.com>
2024-11-29 09:23:40 +0100

    s

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63654c1f0da326239279c835f67f36f37448432b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5e6f66b50bb0787945d478d7c205b30aa1e7c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
