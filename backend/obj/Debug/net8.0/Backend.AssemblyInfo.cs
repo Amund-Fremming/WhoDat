ec7294c9018aebcbbf1f6291b29f72203e7c419f
Amund-fremming <amund.fremming@gmail.com>
2024-11-29 08:17:00 +0100

    merge fuckery

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1a22c6a5752f847be86f0ea1e06acba29d3066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf42cf59ca5582e59a8a20132a4163d11346836")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
