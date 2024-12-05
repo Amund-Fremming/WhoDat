70f256eb30e51137d205d63830ddf14baebbe787
amund.fremming <amund.fremming@gmail.com>
2024-12-05 16:49:20 +0100

    trash

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298c0b92a8b344f4fedc81bacb48c9c6fdffaa0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03eed7287257c261f04014559170611f8213d160")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
